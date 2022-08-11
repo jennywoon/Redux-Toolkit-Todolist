e2e54eacf26a791ab6f0572c1fe3c36332d3af44
jennywoon <cjaeks0394@naver.com>
2022-08-11 17:09:04 +0900

    devTools 추가

--- src/redux/config/configStore.js
+++ src/redux/config/configStore.js
@@ -2,6 +2,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import todos from "../modules/todosSlice";
 
 export const store = configureStore({
+  devTools : process.env.NODE_ENV !== "production",
   reducer: {
 		todos: todos
   },
